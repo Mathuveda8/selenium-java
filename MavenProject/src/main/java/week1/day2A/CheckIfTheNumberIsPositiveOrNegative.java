6799c3b4309cf54f8d14ac680a210548b52ff38d
mathumathi <mathumathi@localhost>
2022-11-09 21:40:36 +0530

    check

---
+++ MavenProject/src/main/java/week1/day2A/CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,36 @@
+package week1.day2A;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number =35;
+		
+		if (number >0) {
+			
+		System.out.println("Given number is Postive");
+			
+		}
+		else if (number<0) {
+			
+		System.out.println("Given number is negetive");
+
+		}
+		else {
+			System.out.println("Enter the valid number");
+		}
+		
+			
+		}
+			
+		
+			
+				
+				
+		
+
+	
+
+	
+
+}
---
+++ MavenProject/src/main/java/week1/day2C/Calculator.java
@@ -0,0 +1,44 @@
+package week1.day2C;
+
+public class Calculator {
+	
+	public int add(int num1,int num2,int num3) {
+		// TODO Auto-generated method stub
+		int result = num1+num2+num3;
+		return result;
+				
+	}
+	public int sub(int num1,int num2) {
+		// TODO Auto-generated method stub
+		int result = num1-num2;
+		return result;
+				
+	}
+	
+	public double mul(double num1,double num2) {
+		// TODO Auto-generated method stub
+		double result = num1 * num2;
+		return result;
+				
+	}
+	
+	public float divide(float num1,float num2) {
+		// TODO Auto-generated method stub
+		float result = num1 / num2;
+		return result;
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+}
